e5ea9f3c9130adaca3ee6b9543e162d690129f67
ArcanineLegend <ericknegrete1@gmail.com>
2018-03-12 20:28:40 -0600

    New Addition Option

--- FormBuilder/src/js/control/addition.js
+++ FormBuilder/src/js/control/addition.js
@@ -1,1 +1,23 @@
-import control from '../control'
+import control from '../control';
+
+export default class controlText extends control {
+  
+  static get definition() {
+    return {
+
+      // mi18n custom mappings (defaults to camelCase type)
+      mi18n: {
+        date: 'dateField',
+        file: 'fileUpload'
+      }
+    };
+  }
+   /**
+   * build a text DOM element, supporting other jquery text form-control's
+   * @return {Object} DOM Element to be injected into the form.
+   */
+  build() {
+    return this.markup('input', null, this.config);
+  }
+}
+control.register('number');
